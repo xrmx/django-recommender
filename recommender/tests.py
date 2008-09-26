2aeb3850e458e0c8e95331b7a18b53f360c24aff
Bosco Curtu <bcurtu@gmail.com>
2008-09-26 16:14:18 +0000

    A bit more interesting test data set
    git-svn-id: http://django-recommender.googlecode.com/svn/trunk@11 6dd71960-3c56-0410-86c2-3fbd0d1e605e

--- recommender/tests.py
+++ recommender/tests.py
@@ -9,8 +9,8 @@ class RecommenderManagerTest(TestCase):
     
     def setUp(self):
         
-        movies = ['Casablanca','Batman begins','2001','Layer Cake','Diamonds forever','Animal House']
-        users = ['John', 'Toby', 'Susan', 'Bosco', 'Harry', 'Helen']
+        movies = ['Casablanca','Batman begins','2001','Layer Cake','Diamonds forever','Animal House','Die Hard','Hitch']
+        users = ['John', 'Toby', 'Susan', 'Bosco', 'Harry', 'Helen','Mike']
         
         for movie in movies:
             TestItem.objects.create(name=movie)
@@ -33,13 +33,17 @@ class RecommenderManagerTest(TestCase):
                 'science-fiction','fx','classic','thriller',
                 'gangsters','action','comedy','thriller',
                 'action', 'adventures', 'thriller', 'classic',
-                'comedy','teenagers','musical','university',]
+                'comedy','teenagers','musical','university',
+                'thriller','ny','action','fx',
+                'comedy','romantic','modern','ny',
+                ]
         user_tags = ['action','adventures','thriller',
                      'romatic','classic','b&w',
                      'romantic','adventures','western',
                      'thriller','gangsters','romantic',
                      'action','fx','gore',
-                     'comedy','classic','b&w',]
+                     'comedy','classic','b&w',
+                     'ny','action','fx',]
         i = 0
         for movie in movies:
             while True:
@@ -80,11 +84,11 @@ class RecommenderManagerTest(TestCase):
             recs = Recommender.objects.get_best_items_for_user(user, users, movies)
             print "Best Items found for %s: %s " % (user, recs)
 
-        user = User.objects.get(id=6)
+        user = User.objects.get(id=3)
         users = Recommender.objects.get_best_items_for_user(user, users, movies)
         self.assertEqual(len(users),2)
-        self.assertEqual(users[0][1].id,6)
-        self.assertEqual(users[1][1].id,1)
+        self.assertEqual(users[0][1].id,7)
+        self.assertEqual(users[1][1].id,6)
 
     def test_get_similar_users(self):
         users = User.objects.all()
@@ -94,10 +98,12 @@ class RecommenderManagerTest(TestCase):
             sim_users = Recommender.objects.get_similar_users(user, users, movies)
             print "Similar users for %s: %s " % (user, sim_users)
 
-        user = User.objects.get(id=1)
+        user = User.objects.get(id=6)
         users = Recommender.objects.get_similar_users(user, users, movies)
-        self.assertEqual(len(users),1)
-        self.assertEqual(users[0][1].id,3)
+        self.assertEqual(len(users),3)
+        self.assertEqual(users[0][1].id,1)
+        self.assertEqual(users[1][1].id,4)
+        self.assertEqual(users[2][1].id,3)
 
     def test_get_best_users_for_item(self):
         users = User.objects.all()
@@ -120,10 +126,11 @@ class RecommenderManagerTest(TestCase):
             sim_movies = Recommender.objects.get_similar_items(movie, users, movies)
             print "Similar movies for %s: %s " % (movie, sim_movies)
         
-        movie = TestItem.objects.get(id=4)
+        movie = TestItem.objects.get(id=1)
         sim_movies = Recommender.objects.get_similar_items(movie, users, movies)
-        self.assertEqual(len(sim_movies),1)
-        self.assertEqual(sim_movies[0][1].id,5)
+        self.assertEqual(len(sim_movies),2)
+        self.assertEqual(sim_movies[0][1].id,7)
+        self.assertEqual(sim_movies[1][1].id,4)
                
     def test_get_content_based_recs(self):
         users = User.objects.all()
