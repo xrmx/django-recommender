cc5cdf860876a7b293df77baf1451671f401ce19
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2012-06-19 16:19:03 +0200

    Remove unneeded imports

--- recommender/models.py
+++ recommender/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 
 from recommender.managers import RecommenderManager
 
