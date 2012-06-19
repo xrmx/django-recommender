c752cd5cdd2f1799560511550a2e3d77905078fb
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2012-06-19 20:03:01 +0200

    Make Recommender model abstract
    Really no need to have this stuff in db in our app, better let
    the developer put it where it feels is better.

--- recommender/models.py
+++ recommender/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 from recommender.managers import RecommenderManager
 
-# Main recommender class. Add here whatever you need to be parametrizable: min values, weigths...
 class Recommender(models.Model):
-
     objects = RecommenderManager()
+
+    class Meta:
+        abstract = True
